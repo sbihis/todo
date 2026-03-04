d9b5d06b06b6260356e2f07ac3d4670a0b7be938
sbihis <71081175+sbihis@users.noreply.github.com>
2026-03-04 23:23:19 +0100

    Update gantt.js

--- 32/gantt.js
+++ 32/gantt.js
@@ -1,7 +1,11 @@
-const diagramCode = document.getElementsByClassName("mermaid")[0];
-diagramCode.innerText = diagramCode.innerText + `
+const gantt = document.getElementsById("gantt");
+gantt.innerText = gantt.innerText + `
 teote :10m
 fsdf : 22m 
 last_task : 9m `
 
-console.log(diagramCode.innerText)
+console.log(gantt.innerText)
+
+gantt.removeAttribute("data-processed");
+mermaid.run({ nodes: [gantt] });
+
